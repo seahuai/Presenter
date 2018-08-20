97c6ec3e4a9c64a5d4675847d435af55ff8bfc77
zhangsihuai <seahuai@foxmail.com>
2018-08-20 19:14:11 +0800

    add header file

--- Presenter/Presenter/ViewController.m
+++ Presenter/Presenter/ViewController.m
@@ -7,33 +7,17 @@
 //
 
 #import "ViewController.h"
-#import "Presenter.h"
-#import "PresenterViewController.h"
-#import "PresenterViewController+Show.h"
-#import "TestViewController.h"
 
 @interface ViewController ()
-
-@property (nonatomic, strong) Presenter *presenter;
 
 @end
 
 @implementation ViewController
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-//    ViewController *viewController = [ViewController new];
-//    viewController.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
-//    _presenter = [Presenter new];
-//    _presenter.option.presentedViewSize = CGSizeMake(100, 200);
-//    [self.presenter presentViewController:viewController inViewController:self];
-    TestViewController *testVC = [[TestViewController alloc] init];
-    [testVC showInViewController:self];
-//    testVC present
-}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 @end
